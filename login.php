2fcc2ae5b84a1760fd29ff5d6e4a6758672d2ab8
DulajMadushanka <dulajgunawardana78@gmail.com>
2018-07-04 22:32:50 +0530

    create login page

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+include "db.php";
+
+session_start();
+
+
+
+if(isset($_POST["userLogin"])){
+	$email = mysqli_real_escape_string($con,$_POST["userEmail"]);
+	$passwords = md5($_POST["userPassword"]);
+	
+	
+	$sql = "SELECT * FROM user_info WHERE email = '$email' AND password = '$passwords'";
+	$run_query = mysqli_query($con,$sql);
+	$count = mysqli_num_rows($run_query);
+	
+	if($count == 1){
+		$row = mysqli_fetch_array($run_query);
+		
+		$_SESSION["uid"] = $row["user_id"];
+		$_SESSION["name"] = $row["first_name"];
+		echo "truevghgvgf";
+		
+	}
+}
+?>
